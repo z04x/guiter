66ddeba2d37f0a1db4abc305f7252bcdd2db5814
z04x <niknpetrik@gmail.com>
2022-07-13 18:46:14 +0300

    addStyleAndJs git push git commit -m addStyleAndJs

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use script"
+
+const reviewsSwiper = document.querySelector('.swiper-reviews');
+
+if (reviewsSwiper) {
+    const swiper = new Swiper('.swiper-reviews', {
+        // Optional parameters
+        autoHeight: true,
+        loop: true,
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+    });
+}
